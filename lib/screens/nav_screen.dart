a24ac66072e57aa557ce4b83b15563949f8e599a
Omkar2620 <otbhostekar@gmail.com>
2021-05-25 02:30:14 +0530

    bottom nav and sliver app bar done

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube_ui/screens/home_screen.dart';
 
 class NavScreen extends StatefulWidget {
   @override
@@ -6,8 +7,74 @@ class NavScreen extends StatefulWidget {
 }
 
 class _NavScreenState extends State<NavScreen> {
+  int _selectedIndex = 0;
+  final _screens = [
+    HomeScreen(),
+    const Scaffold(
+      body: Center(
+        child: Text('Explore'),
+      ),
+    ),
+    const Scaffold(
+      body: Center(
+        child: Text('Ad'),
+      ),
+    ),
+    const Scaffold(
+      body: Center(
+        child: Text('Subscriptions'),
+      ),
+    ),
+    const Scaffold(
+      body: Center(
+        child: Text('Library'),
+      ),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: _screens
+            .asMap()
+            .map((i, value) => MapEntry(
+                i,
+                Offstage(
+                  offstage: _selectedIndex != i,
+                  child: value,
+                )))
+            .values
+            .toList(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        selectedFontSize: 10.0,
+        unselectedFontSize: 10.0,
+        onTap: (i) => setState(() => _selectedIndex = i),
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              activeIcon: Icon(Icons.home),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.explore_outlined),
+              activeIcon: Icon(Icons.explore),
+              label: 'Explore'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle_outline),
+              activeIcon: Icon(Icons.add_circle),
+              label: 'Add'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.subscriptions_outlined),
+              activeIcon: Icon(Icons.subscriptions),
+              label: 'Subscriptions'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.video_library_outlined),
+              activeIcon: Icon(Icons.video_library),
+              label: 'Library'),
+        ],
+      ),
+    );
   }
 }
